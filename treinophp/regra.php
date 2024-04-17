a2f3f51a32de2ec8c5ca8c9015b3e3277b628ede
Matheus Alves <164942311+UmDevManauara@users.noreply.github.com>
2024-04-17 09:33:03 -0400

    treino

--- treinophp/regra.php
+++ treinophp/regra.php
@@ -17,8 +17,8 @@
         <label for="numero2">numero2:</label>
         <input type="number" name="numero2" id="numero2">
         <br>
-        <input type="submit">
-    </form>
+       //
+    
     <?php
     $variavel1 = $_POST['numero1'];
     $variavel2 = $_POST['numero2'];
@@ -42,31 +42,32 @@
 
     <h2>Comparando strings</h2>
     <form action="<?= $_SERVER['PHP_SELF'] ?>" method="post">
-        <label for="numero1">numero 1:</label>
-        <input type="number" name="numero1" id="numero1">
+        <label for="string1">string 1:</label>
+        <input type="text" name="string1" id="string1">
         <br>
-        <label for="numero2">numero2:</label>
-        <input type="number" name="numero2" id="numero2">
+        <label for="string2">string 2:</label>
+        <input type="text" name="string2" id="string2">
         <br>
         <input type="submit">
+        
+
     </form>
     <?php
-    $variavel1 = $_POST['numero1'];
-    $variavel2 = $_POST['numero2'];
-    
-   if   // 1 regra: se X é maior y faça ISSO, senão ISSO:
-        ($variavel1 > $variavel2){
-        echo "<p>$variavel1 é maior que o $variavel2</p>";
-
-    } // 1 regra: se x é menor y faça ISSO, senão ISSO:
-        else if($variavel1 < $variavel2){
-        echo "<p>$variavel1 é menor que o $variavel2</p>";
-    }
+    $string1 = $_POST['string1'];
+    $string2 = $_POST['string2'];
     
     if //3 regra: se X é iguala y
-        ($variavel1 == $variavel2){
-        echo "<p>o primeiro numero:<strong>$variavel1</strong> é igual ao valor do segundo numero:<strong>$variavel2</strong></p>";
+        ($string1 == $string2){
+        echo "<p>a primeira string:<strong>$string1</strong> é semelhante a segunda string:<strong>$string2</strong></p>";
+    }//4 regra: se X é diferente de y
+        else {
+        echo "<p>a primeira string:<strong>$string1</strong> é DIFERENTE da segunda string:<strong>$string2</strong></p>";
     }
+    var_dump($_REQUEST);
+    //SOMANDO OS INPUTS
+    echo "<h2>Somando os inputs</h2>";
+    $conta = 0;
+
 
     ?>
 
